9032ea4e19da7b0f56a85deb8d29280c846a823e
vilnar <yura.gagarin9@gmail.com>
2022-09-08 22:06:32 +0300

    fix indexer

--- manticore/init.sh
+++ manticore/init.sh
@@ -3,7 +3,8 @@
 sudo mkdir -p /var/log/manticore
 sudo chown -R manticore:manticore /var/run/manticore /var/lib/manticore /var/log/manticore /home/manticore/
 
-searchd --config /etc/manticoresearch/manticore.conf && indexer --rotate --all
+indexer --all
+searchd --config /etc/manticoresearch/manticore.conf
 
 
 printf '\n\nPrepare environment - Done!\n\n'
